52884c358db278c70af1d8cd030ca4081d982a38
miketaoma <53985260+miketaoma@users.noreply.github.com>
2024-04-25 23:51:19 +0000

    +API routes for delete and add function. FIXME: How to distinguish between add and update?

--- index.js
+++ index.js
@@ -18,51 +18,87 @@ app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
-    Movie.find({}).lean()
-        .then((movies) => {
-          res.render('home', { movies });
-        })
-        .catch(err => next(err));
+app.get('/', (req,res,next) => {
+  Movie.find({}).lean()
+    .then((movies) => {
+      res.render('home', { movies });
+    })
+    .catch(err => next(err));
 });
 
 app.get('/details/:title', (req,res,next) => {
-    Movie.findOne({ title:req.params.title }).lean()
-        .then((movie) => {           
-          res.render('details', {result: movie} );
-        })
-        .catch(err => next(err));
+  Movie.findOne({ title:req.params.title }).lean()
+    .then((movie) => {           
+      res.render('details', {result: movie} );
+    })
+    .catch(err => next(err));
 });
 
 //API for all items
 app.get('/api/movies', (req,res) => {
-    Movie.find({}).lean()
-        .then((movies) => {
-          res.json(movies);
-        })
+  Movie.find({}).lean()
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Database Error occurred');
+    });
 });
 
 //API for one item by title (case senstive)
 app.get('/api/movies/:title', (req,res) => {
-    Movie.findOne({ title:req.params.title }).lean()
-        .then((movie) => {           
-          res.json(movie);
-        })
+  Movie.findOne({ title:req.params.title }).lean()
+    .then((movie) => {
+      if (movie) {
+        res.json(movie);
+      } else {
+        res.status(200).json('Movie not found with title: ' + req.params.title);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Database Error occurred');
+    });
 });
 
 //API for deleting item
-
 app.get( '/api/delete/:title', (req,res) => {   
-    Movie.deleteOne({ title:req.params.title }).lean()
-        .then((movie) => {           
-          res.json(movie);
-        })
+  Movie.deleteOne({ title:req.params.title }).lean()
+    .then((movie) => {           
+      if (movie.deletedCount > 0) {
+        res.json(req.params.title + ' deleted');
+      } else {
+        res.json(req.params.title + ' not found. Deletion failed.');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Database Error occurred');
+    });
 });
 
 //API for adding item
-//app.post ( '/api/add/:title', (req,res) => {
+app.post ( '/api/add/:title/:director/:year/:genre', (req,res) => {
+  let title = req.params.title;
+  let director = req.params.director;
+  let year = req.params.year;
+  let genre = req.params.genre;
   
-//});
+  Movie.findOne({ title, director, year, genre })
+    .then((movie) => {
+      if (movie) {
+        res.json(req.params.title + ' already exists'); 
+      } else {
+        Movie.create({ title, director, year, genre })
+        res.json(req.params.title + ' added');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Database Error occurred');
+    });
+});
 
 app.use((req,res) => {
     res.type('text/plain'); 
